52339ec0a92a638573cb635ee5bafc00139c832e
Manas Hejmadi <manas2all@outlook.com>
2023-12-14 13:33:21 +0530

    Added more Models such as Product, Manufacturer etc

--- TrashHubBackend/TrashHubBackend/models.py
+++ TrashHubBackend/TrashHubBackend/models.py
@@ -49,3 +49,46 @@ class BinoccularDustbin(db.Model):
 			'lat': self.lat,
 			'lng': self.lng,
 		}
+
+# ============================ (TrashTag) ================================
+
+class TrashTagVendor(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String)
+	username = db.Column(db.String)
+	password = db.Column(db.String)
+	points = db.Column(db.Float, default=0.0)
+
+	def __init__(self, name, username,password):
+		self.name = name
+		self.username = username
+		self.password = password
+
+
+class TrashTagManufacturer(db.Model):
+	__tablename__ = 'trashtag_manufacturer'
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String)
+	username = db.Column(db.String)
+	password = db.Column(db.String)
+
+	#[relationships]
+	products = db.relationship('TrashTagProduct', backref='manufacturer')
+
+	def __init__(self, name, username, password):
+		self.name = name
+		self.username = username
+		self.password = password
+
+
+class TrashTagProduct(db.Model):
+	__tablename__ = 'trashtag_product'
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String)
+	batches = db.relationship('ProductBatch', backref='product', cascade='delete')
+
+	manufacturer_id = db.Column(db.Integer, db.ForeignKey('trashtag_manufacturer.id'))
+
+	def __init__(self, name, manufacturer):
+		self.name = name
+		self.manufacturer = manufacturer
