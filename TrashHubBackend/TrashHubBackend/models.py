e0ed89279dfffc896c8236b89e9563b9f2571da2
Manas Hejmadi <manas2all@outlook.com>
2023-12-14 13:39:17 +0530

    RCXPartner, RCXJob and other models created

--- TrashHubBackend/TrashHubBackend/models.py
+++ TrashHubBackend/TrashHubBackend/models.py
@@ -92,3 +92,90 @@ class TrashTagProduct(db.Model):
 	def __init__(self, name, manufacturer):
 		self.name = name
 		self.manufacturer = manufacturer
+
+
+class ProductBatch(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	product_id = db.Column(db.Integer, db.ForeignKey('trashtag_product.id'))
+
+	def __init__(self, product):
+		self.product = product
+
+	#relationships
+	entities = db.relationship('ProductEntity', backref='batch', cascade='delete')
+
+	def get_disposed_count(self):
+		count = 0
+		for entity in self.entities:
+			if(entity.disposed):
+				count += 1
+		return count
+	
+	def get_purchased_count(self):
+		count = 0
+		for entity in self.entities:
+			if(entity.purchased):
+				count += 1
+		return count
+
+	#[backref]
+	#product
+
+class ProductEntity(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	batch_id = db.Column(db.Integer, db.ForeignKey('product_batch.id'))
+	disposed = db.Column(db.Boolean, default=False)
+	purchased = db.Column(db.Boolean, default=False)
+
+	def __init__(self, batch):
+		self.batch = batch
+
+	#[backref]
+	#batch
+
+# ============================ (ReCyclX) ================================
+
+
+class RCXPartner(db.Model):
+	__tablename__ = 'rcx_partner'
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String)
+	type = db.Column(db.String)
+	username = db.Column(db.String)
+	password = db.Column(db.String)
+
+	jobs = db.relationship('RCXJob', backref='partner')
+
+	def __init__(self, name, type, username, password):
+		self.name = name
+		self.type = type
+		self.username = username
+		self.password = password
+
+class RCXJob(db.Model):
+	__tablename__ = 'rcx_job'
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String)
+	status = db.Column(db.String)
+	date = db.Column(db.DateTime, default=datetime.now)
+	lat = db.Column(db.Float) # User Location 
+	lng = db.Column(db.Float) # User Location 
+
+	#[foreignkeys]
+	partner_id = db.Column(db.Integer, db.ForeignKey('rcx_partner.id'))
+	client_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+	#[backrefs]
+	#partner = <Partner>
+	#client = <User>
+
+	def __init__(self, name, status, lat, lng, client, partner):
+		self.name = name
+		self.status = status
+		self.lat = lat
+		self.lng = lng
+		self.client = client
+		self.partner = partner
+
+	def __repr__(self):
+		return f'RCXJob({self.name}, {self.status}, {self.client}, {self.partner})'
