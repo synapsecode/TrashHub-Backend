d66aabe53b93d0055a21a75347dae8f8eba29ce0
Manas Hejmadi <manas2all@outlook.com>
2023-12-14 13:31:10 +0530

    User & PrimitiveBinOcculars model created

--- TrashHubBackend/TrashHubBackend/models.py
+++ TrashHubBackend/TrashHubBackend/models.py
@@ -1,8 +1,51 @@
 #Database Layer
 from datetime import datetime
-from flask import current_app
+from typing import Dict
+from sqlalchemy.ext.hybrid import hybrid_method
 from TrashHubBackend import db
 
-class TestModel(db.Model):
+class User(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
-	test_name = db.Column(db.String)
+	name = db.Column(db.String)
+	username = db.Column(db.String)
+	password = db.Column(db.String)
+
+	points = db.Column(db.Float, default=0.0)
+
+	# Relationships
+	RCXJobs = db.relationship('RCXJob', backref='client')
+	
+
+	def __repr__(self):
+		return f"User({self.name}, {self.username})"
+	
+	def __init__(self, name, username, password):
+		self.name = name
+		self.username = username
+		self.password = password
+
+# ============================ (BinOcculars) ================================
+
+class BinoccularDustbin(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String)
+	type = db.Column(db.String)
+	lat = db.Column(db.Float)
+	lng = db.Column(db.Float)
+
+	def __repr__(self):
+		return f"BinOccularDusbin({self.name}, {self.name}, ({self.lat}, {self.lng}))"
+	
+	def __init__( self, name='default', type='regular', lat=0.0, lng=0.0):
+		self.name = str(name)
+		self.type = str(type)
+		self.lat = float(str(lat))
+		self.lng = float(str(lng))
+
+	def toJson(self) -> Dict:
+		return {
+			'name': self.name,
+			'type': self.type,
+			'lat': self.lat,
+			'lng': self.lng,
+		}
